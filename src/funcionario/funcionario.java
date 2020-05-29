3700b9bf4e44903ae6f382f1e375fc9f732b5fb2
Luiz Felipe <luizdoid.fl@hotmail.com>
2020-05-29 20:10:29 -0300

    Leitura dos dados

--- src/funcionario/funcionario.java
+++ src/funcionario/funcionario.java
@@ -7,6 +7,34 @@ public class funcionario {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 
+		System.out.printf("Digitar outro (S/N)? ");
+		digitar = sc.next().charAt(0);
+
+		while (digitar == 's' || digitar == 'S') {
+			System.out.printf("Nome: ");
+			nome = sc.next();
+
+			System.out.printf("Horas trabalhadas: ");
+			horastrabalhadas = sc.nextDouble();
+
+			while (horastrabalhadas < 0) {
+				System.out.printf("Horas trabalhadas: ");
+				horastrabalhadas = sc.nextDouble();
+			}
+
+			System.out.printf("Valor por hora: ");
+			valorporhora = sc.nextDouble();
+
+			while (valorporhora < 0) {
+				System.out.printf("Valor por hora: ");
+				valorporhora = sc.nextDouble();
+			}
+			System.out.printf("Digitar outro (S/N)? ");
+			digitar = sc.next().charAt(0);
+
+		}
+	
+		
 		
 		
 		sc.close();
